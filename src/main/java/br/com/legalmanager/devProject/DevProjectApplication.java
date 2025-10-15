9ce66d6b20eb54bff282e0421655faf6af405671
Douglas Lima <46324875+Gigalima@users.noreply.github.com>
2025-10-14 22:17:31 -0300

    Update DevProjectApplication.java
    Feito import

--- src/main/java/br/com/legalmanager/devProject/DevProjectApplication.java
+++ src/main/java/br/com/legalmanager/devProject/DevProjectApplication.java
@@ -2,6 +2,7 @@ package br.com.legalmanager.devProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DevProjectApplication extends SpringBootServletInitializer {
